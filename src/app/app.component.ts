ae46b56e02b8e024097e9cb8ae07200b1becedf7
Monish Narwani <monish.narwani@focalworks.in>
2018-04-08 13:45:44 +0530

    how css imports styles work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './servers/servers.component.css'],
+  styles: [`
+      h3 {
+          color: blanchedalmond;
+      }
+  `],
 })
 export class AppComponent {
   title = 'Monish Narwani';
